5a8a89231eadcb068c4fce66092665aeb1627541
Martin Sršeň <martin@srsen.sk>
2021-03-08 22:04:21 +0100

    Updated password to hash

--- migrations/Version20210307194154.php
+++ migrations/Version20210307194154.php
@@ -20,8 +20,8 @@ final class Version20210307194154 extends AbstractMigration
         $this->addSql("INSERT INTO role (id, name, code) VALUES (1, 'User', 'ROLE_USER');");
         $this->addSql("INSERT INTO role (id, name, code) VALUES (2, 'Administrator', 'ROLE_ADMIN');");
 
-        // Add default user
-        $this->addSql("INSERT INTO user (id, role_id, first_name, last_name, email, password) VALUES (1, 2, 'John', 'Smith', 'john.smith@example.com', 'password123');");
+        // Add default user (password: password123)
+        $this->addSql("INSERT INTO user (id, role_id, first_name, last_name, email, password) VALUES (1, 2, 'John', 'Smith', 'john.smith@example.com', 'bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf');");
     }
 
     public function down(Schema $schema) : void
